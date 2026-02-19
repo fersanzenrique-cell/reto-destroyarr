b299fc61b0f18ddc34bb8eaf23f1fb1d3b9b6663
Enrique <fersanzenrique@gmail.com>
2026-02-19 19:26:03 +0100

    6650

--- java/2026/feb/Seisseiscinco.java
+++ java/2026/feb/Seisseiscinco.java
@@ -1,1 +1,7 @@
-public class Seisseiscinco {}
+import java.util.Scanner;
+
+public class Seisseiscinco {
+  static Scanner in = new Scanner(System.in);
+
+  public static void main(String[] args) {}
+}
